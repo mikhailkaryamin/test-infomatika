aab45d161d671c0ca626747551ba728c47c50f80
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-08-26 20:42:44 +0500

    feat(header): compelete header-desktop, RC with style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,40 +1,60 @@
 import React from 'react';
 
+const SITE_MENU_ITEMS = ['Как купить?', 'Правила', 'Возврат билета'];
+const USER_NAVIGATION_ITEMS = [
+  {
+    prefix: 'sign-in',
+    title: 'Войти',
+    isHiddenTitle: false,
+  },
+  {
+    prefix: 'menu',
+    title: 'Меню',
+    isHiddenTitle: true,
+  },
+];
+
+function getMarkupSiteMenu() {
+  return (
+    SITE_MENU_ITEMS.map((item) => (
+      <li key={item} className="site-navigation__item">
+        <a className="header-link" href="/">
+          {item}
+        </a>
+      </li>
+    ))
+  );
+}
+
+function getMarkupUserMenu() {
+  return (
+    USER_NAVIGATION_ITEMS.map((item) => (
+      <li key={item.prefix} className={`user-navigation__item user-navigation__item--${item.prefix}`}>
+        <a href="/" className={`header-link user-navigation__link user-navigation__link--${item.prefix}`}>
+          <svg className="header-link__icon user-navigation__sign-in-icon" width="24" height="24" viewBox="0 0 24 24">
+            <use xlinkHref={`#${item.prefix}`} />
+          </svg>
+          <span className={item.isHiddenTitle ? 'visually-hidden' : ''}>
+            {item.title}
+          </span>
+        </a>
+      </li>
+    ))
+  );
+}
+
 function Header() {
   return (
     <header className="header">
-      <a href="#" className="logo">
+      <a href="/" className="logo header-link">
         Билеты и абонементы
       </a>
       <nav className="header__navigation">
-        <ul className="site-navigation">
-          <li className="site-navigation__item">
-            <a href="#">
-              Как купить?
-            </a>
-          </li>
-          <li className="site-navigation__item">
-            <a href="#">
-              Правила
-            </a>
-          </li>
-          <li className="site-navigation__item">
-            <a href="#">
-              Возврат билета
-            </a>
-          </li>
+        <ul className="site-navigation header__list">
+          {getMarkupSiteMenu()}
         </ul>
-        <ul className="user-navigation">
-          <li className="user-navigation__item">
-            <a href="#">
-              Войти
-            </a>
-          </li>
-          <li className="user-navigation__item">
-            <a href="#">
-              Меню
-            </a>
-          </li>
+        <ul className="user-navigation header__list header__list--user-menu">
+          {getMarkupUserMenu()}
         </ul>
       </nav>
     </header>

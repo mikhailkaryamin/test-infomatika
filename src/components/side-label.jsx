2f5dec391e04441dc9a31c8f858bc8fc9dd23736
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-03 22:08:37 +0500

    feat(side-label): new component

--- src/components/side-label.jsx
+++ src/components/side-label.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { string } from 'prop-types';
 
-export default function SideLabel({ teamName }) {
+export default function SideLabel({ teamName, prefix }) {
   return (
-    <div className="side-label">
+    <div className={`side-label side-label--${prefix}`}>
       {teamName}
     </div>
   );
 }
 
 SideLabel.propTypes = {
+  prefix: string.isRequired,
   teamName: string.isRequired,
 };
